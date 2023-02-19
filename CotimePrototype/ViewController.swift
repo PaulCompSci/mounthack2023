ddd74a0cf0fc1943ae44c162e42a4246ecbfabdc
paul <kohleangpaul911@gmail.com>
2023-02-19 09:46:11 -0800

    Initial Commit

---
+++ CotimePrototype/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CotimePrototype
+//
+//  Created by Paul on 2023-02-19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
